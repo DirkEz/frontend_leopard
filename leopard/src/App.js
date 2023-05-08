2b19a66843b5f3bc6d2db55da786f579a39ae191
DirkEz <dirk.schaafstra@gmail.com>
2023-05-08 11:42:27 +0200

    App Updated

--- leopard/src/App.js
+++ leopard/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="main">
+          <div class="menu-bar">
+            <div class="name-bar">
+              {/* <div class="items"> */}
+                <img src={logo} className="App-logo" alt="logo" width="150px" />
+                <p>Leopard</p>
+              {/* </div> */}
+            </div>
+            {/* <ul>
+                <li>Home</li>
+                <li>Zoeken</li>
+                <li>Chat</li>
+                <li>Profiel</li>
+        </ul> */}
+          </div>
+          <div className="view-story"></div>
+          <div className="view-feed"></div>
+          <div className="friend-list"></div>
+        </div>
       </header>
     </div>
   );
